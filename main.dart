547b662249f972253e2f3b5c64832f42bcfb3d8a
roaahatan <58633999+roaahatan@users.noreply.github.com>
2020-01-08 19:19:25 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+      setState(() {
+
+        _counter++;
+      });
+
+  }
+  void _decrementCounter() {
+    setState(() {
+
+      _counter--;
+    });
+
+  }
+  void _resetCounter() {
+    setState(() {
+
+      _counter= 0;
+    });
+
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text("tapping game"),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            RaisedButton(
+              onPressed: (){
+                _incrementCounter();
+              },
+              child: Text('+'),),
+
+            RaisedButton(
+              onPressed: (){
+                _decrementCounter();
+
+
+              },
+           child: Text('-'),
+
+            ),
+            RaisedButton(
+              onPressed: (){
+                _resetCounter();
+
+
+              },
+              child: Text('reset'),
+            ),
+
+          ],
+
+        ),
+      ),
+
+
+
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+
+
+
+  }
+
+
+}
